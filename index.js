3f76b69ce51cd0402bdd08a18e57366643ab2611
Conrad <cnguyen3@bluecatnetworks.com>
2022-04-27 11:40:59 +0700

    second_commit

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 import express from "express";
 import cors from 'cors'
 import bodyParser from 'body-parser'
-
+import http from 'http'
+import { Server } from "socket.io";
 
 const app = express()
 const PORT = process.env.port || 3002
-
 app.use(cors())
+// =========================================
+
+
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3001",
+        methods: ["GET", ["POST"]]
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log("User connected: ", socket.id);
+    socket.on("join_room", (data) => {
+        socket.join(data)
+        console.log(`User with Id: ${socket.id} joined the room: ${data}`);
+    })
+
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data)
+    })
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    })
+})
 
 app.use('/login', (req, res) => {
     res.send({
@@ -14,6 +40,7 @@ app.use('/login', (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
+
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
